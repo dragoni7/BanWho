cc0122570102124fb370cbc7accc24d27302288b
dragoni7 <sdgibson18@outlook.com>
2024-03-24 10:32:18 -0700

    Reduced sample size

--- BanWho.Infrastructure/Jobs/UpdateChampGameStatsBackgroundJob.cs
+++ BanWho.Infrastructure/Jobs/UpdateChampGameStatsBackgroundJob.cs
@@ -57,7 +57,7 @@ internal class UpdateChampGameStatsBackgroundJob : IJob
 	public async Task SeedChampGameStatsAsync()
 	{
 		Player[] allPlayers = await _playerPuuidRepository.GetAllAsync();
-		Player[] playerPuuids = Random.Shared.GetItems(allPlayers, allPlayers.Length / 2);
+		Player[] playerPuuids = Random.Shared.GetItems(allPlayers, allPlayers.Length / 4);
 
 		_logger.LogInformation($"Using sample of {playerPuuids.Length} players");
 
